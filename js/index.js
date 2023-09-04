e48fe14ac268b9d6dd7e350282861e28bc7231c5
KennyOwO <spkennypro@gmail.com>
2023-09-04 14:43:21 +0800

    added fade effect

--- js/index.js
+++ js/index.js
@@ -133,3 +133,21 @@ window.addEventListener("scroll", function () {
         list[4].classList.add('active');
     }
   });
+
+  // reveal
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('displayed')
+        } else {
+            reveals[i].classList.remove('displayed')
+        }
+    }
+}
